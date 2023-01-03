ffd38f386f73d2511ed25c75b8236e7d9fb78ef3
Roberto Queiroz <60795763+khanqz@users.noreply.github.com>
2023-01-03 14:39:16 -0300

    Barra de navegação
    Criada uma barra de navegação responsiva no site!

--- site-elementos/javascript.js
+++ site-elementos/javascript.js
@@ -1,12 +1,15 @@
 
-alert("Hello World!")
+const nav = document.querySelector(".elementos");
+const botao = document.querySelector(".linhas");
+const linhas = document.querySelectorAll(".span");
 
-var nome = "Roberto"
-var idade = "31"
-var peso = "80kg"
+function alternar() {
+    nav.classList.toggle("barra-lateral");
+    linhas[0].classList.toggle("linha-0");
+    linhas[1].classList.toggle("linha-1");
+    linhas[2].classList.toggle("linha-2"); 
 
-document.write(`O novo usuário é ${nome}, tem ${idade} anos e pesa ${peso} Kilos!`)
+  }
 
-
-
-
+  botao.addEventListener("click", alternar);
+  
